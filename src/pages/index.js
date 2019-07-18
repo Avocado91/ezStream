9d4ee8c66da0a8300be788f1da3e2d600235e9d4
T.J <t.ruggles@live.ca>
2019-07-18 16:13:29 -0400

    Setting up basic movie api call

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,37 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 
-const IndexPage = () => (
-    <Layout>
-        <Link to="/series/">Go to Series</Link>
-    </Layout>
-)
+class IndexPage extends React.Component {
+    state = {
+        movie: undefined,
+    }
+
+    componentDidMount() {
+        this.getMovies()
+    }
+
+    getMovies = async () => {
+        const apiCall = await fetch(
+            `https://api.themoviedb.org/3/movie/550?api_key=9c2eaa5059825028da6ae6210bb7a7d2`
+        )
+
+        const response = await apiCall.json()
+
+        this.setState({
+            movie: response.original_title,
+        })
+
+        console.log(response.original_title)
+    }
+
+    render() {
+        return (
+            <Layout>
+                <Link to="/series/">Go to Series</Link>
+                <h2>{this.state.movie}</h2>
+            </Layout>
+        )
+    }
+}
 
 export default IndexPage
